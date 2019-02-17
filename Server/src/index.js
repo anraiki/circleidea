2a0c862f843a16e42a2df4bbe2f17b3f0eba1035
Henry <anraiki@gmail.com>
2019-02-17 05:07:30 -0800

    Connect Postgres to Node and add PG dependency

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,7 +1,20 @@
-const express = require('express');
-const app = express();
-const port = 8000;
+const express       = require('express');
+const { Client }    = require("pg");
+const app           = express();
+const port          = 8000;
 
-app.get('/api', (req, res) => res.send('Hello World!'));
+let db              = new Client();
+
+
+app.get('/api', async (req, res) => {
+
+    await db.connect();
+    const result = await db.query('SELECT NOW()');
+    console.log(result);
+    await db.end();
+
+    res.send('Hello World!');
+
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
