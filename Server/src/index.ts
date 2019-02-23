c888df6205e8e112bb0d34d72b4c14d6591ead0c
Henry <anraiki@gmail.com>
2019-02-23 13:19:19 -0800

    Conforming to Typescript. Add typescript dependency of PG and Express

---
+++ Server/src/index.ts
@@ -0,0 +1,19 @@
+const express       = require("express");
+const { Client }    = require("pg");
+const app           = express();
+const port          = 8000;
+
+let db              = new Client();
+
+app.get("/api", async (req, res) => {
+
+    await db.connect();
+    const result = await db.query("SELECT NOW()");
+    console.log(result);
+    await db.end();
+
+    res.send("Hello World!");
+
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
